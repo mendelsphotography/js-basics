c138cd73053cbc8af6ed611675dc771a9d7e2113
Mendelsphotography <mendelsphotography@gmail.com>
2022-08-05 07:52:50 -0400

    First Commit Start with index.html, index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+  console.log("hello world");
+  
+  let name = 'me';
+  console.log (name);
+
+  // Rules for js variables
+  // cannot be a reserved keyword
+  // Shuld be meaningful
+  // Canon start with a number (1name)
+  //Cannot contain a space or hyphen (-)
+  // Are case-sensitive 
